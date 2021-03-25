ea23782b45fd12f4e6e534e782a458fd42999151
yundeveloper <yuniorbatista1113@gmail.com>
2021-03-25 18:04:50 -0400

    validation for empty input box before submission has been added.  Also, logo is now clickable and it will take you to homepage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 function Navigation({ updateSearchQuery }) {
   const [querySearch, setQuerySearch] = useState('');
 
+  const pageReload = () => {
+    window.location.reload(false);
+  };
+
   const formSubmit = (e) => {
-    e.preventDefault();
-    updateSearchQuery(querySearch);
-    setQuerySearch('');
+    if (querySearch.length > 0) {
+      e.preventDefault();
+      updateSearchQuery(querySearch);
+      setQuerySearch('');
+    }
   };
 
   return (
@@ -14,6 +20,7 @@ function Navigation({ updateSearchQuery }) {
       <img
         src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
         alt="TMDB logo"
+        onClick={pageReload}
       />
       <form onSubmit={formSubmit}>
         <input
