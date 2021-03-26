e3b195a13bcb30a1451a530e1cc5b03299a04bd9
yundeveloper <yuniorbatista1113@gmail.com>
2021-03-25 21:44:00 -0400

    color changing on averate vote rating has been added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const IMAGE_API = `https://image.tmdb.org/t/p/w1280`;
 
 export default function Movie({ title, overview, poster_path, vote_average }) {
+  const changeColor = (val) => {
+    return val >= 8 ? 'green' : 'red';
+  };
+
   return (
     <div className="movie">
       <img src={IMAGE_API + poster_path} alt={title} />
 
       <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={changeColor(vote_average)}>{vote_average}</span>
       </div>
       <div className="overview">
         <h2>Overview:</h2>
